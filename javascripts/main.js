f13840e96024ebad397f48c4f9a60e86fee433c0
anniebae <bae.annie@gmail.com>
2015-02-26 18:05:17 -0500

    fade effect put into carousel

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,53 +1,53 @@
 $(function() {
 
-  $('.anniebae').mouseenter(function(){
-    $(this).animate({
-      height: "85px",
-      width: "85px"
-    });
-  });
+  // $('.anniebae').mouseenter(function(){
+  //   $(this).animate({
+  //     height: "85px",
+  //     width: "85px"
+  //   });
+  // });
 
-  $('.anniebae').mouseleave(function(){
-    $(this).animate({
-      height: "80px",
-      width: "80px"
-    });
-  });
+  // $('.anniebae').mouseleave(function(){
+  //   $(this).animate({
+  //     height: "80px",
+  //     width: "80px"
+  //   });
+  // });
 
-  $('.project-photo').hover(function(){
-    $('.project-caption', this).stop(true, true).slideDown("normal");
-  }, function(){
-    $('.project-caption', this).stop(true, true).hide();
-  });
-
-
-  $('.resume-photo').hover(function(){
-    $('.resume-caption', this).stop(true, true).slideDown("normal");
-  }, function(){
-    $('.resume-caption', this).stop(true, true).hide();
-  });
+  // $('.project-photo').hover(function(){
+  //   $('.project-caption', this).stop(true, true).slideDown("normal");
+  // }, function(){
+  //   $('.project-caption', this).stop(true, true).hide();
+  // });
 
 
+  // $('.resume-photo').hover(function(){
+  //   $('.resume-caption', this).stop(true, true).slideDown("normal");
+  // }, function(){
+  //   $('.resume-caption', this).stop(true, true).hide();
+  // });
 
-  $('#projects-tab').on('click', function() {
-    $(this).parent().find('li.active').removeClass('active');
-    $(this).addClass('active');
-  });
 
-  $('#resume-tab').on('click', function() {
-    $(this).parent().find('li.active').removeClass('active');
-    $(this).addClass('active');
-  });
 
-  $('#photography-tab').on('click', function() {
-    $(this).parent().find('li.active').removeClass('active');
-    $(this).addClass('active');
-  });
+  // $('#projects-tab').on('click', function() {
+  //   $(this).parent().find('li.active').removeClass('active');
+  //   $(this).addClass('active');
+  // });
 
-  $('#contacts-tab').on('click', function() {
-    $(this).parent().find('li.active').removeClass('active');
-    $(this).addClass('active');
-  });
+  // $('#resume-tab').on('click', function() {
+  //   $(this).parent().find('li.active').removeClass('active');
+  //   $(this).addClass('active');
+  // });
+
+  // $('#photography-tab').on('click', function() {
+  //   $(this).parent().find('li.active').removeClass('active');
+  //   $(this).addClass('active');
+  // });
+
+  // $('#contacts-tab').on('click', function() {
+  //   $(this).parent().find('li.active').removeClass('active');
+  //   $(this).addClass('active');
+  // });
 
   $('body').scrollspy({ target: '.navbar' });
 
