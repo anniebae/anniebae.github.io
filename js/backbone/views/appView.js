bf0947031fd13400dd33517bef3542022331a638
anniebae <bae.annie@gmail.com>
2015-02-26 23:32:53 -0500

    views rendering in backbone

--- js/backbone/views/appView.js
+++ js/backbone/views/appView.js
@@ -1,6 +1,11 @@
 var AppView = Backbone.View.extend({
   el: "#logo-navbar",
   logoNavbarTemplate: _.template($("#logo-navbar-template").html()),
+  mainTemplate: _.template($('#main-template').html()),
+  projectsTemplate: _.template($("#projects-template").html()),
+  resumeTemplate: _.template($("#resume-template").html()),
+  photographyTemplate: _.template($("#photography-template").html()),
+  contactsTemplate: _.template($("#contacts-template").html()),
   initialize: function(){
     this.renderLogoNavbar();
     this.showMain();
@@ -16,4 +21,20 @@ var AppView = Backbone.View.extend({
     this.$el.html(this.logoNavbarTemplate());
     return this;
   },
+  showMain: function() {
+    this.renderLogoNavbar();
+    $('#body').html(this.mainTemplate());
+  },
+  showProjects: function() {
+    $('#body').html(this.projectsTemplate());
+  },
+  showResume: function() {
+    $('#body').html(this.resumeTemplate());
+  },
+  showPhotography: function() {
+    $('#body').html(this.photographyTemplate());
+  },
+  showContacts: function() {
+    $('#body').html(this.contactsTemplate)
+  }
 });
--- js/backbone/views/mainView.js
+++ js/backbone/views/mainView.js
@@ -1,10 +1,5 @@
 var MainView = Backbone.View.extend({
   el: "#main",
-  landingTemplate: _.template($('#landing-template').html()),
-  projectsTemplate: _.template($("#projects-template").html()),
-  resumeTemplate: _.template($("#resume-template").html()),
-  photographyTemplate: _.template($("#photography-template").html()),
-  contactsTemplate: _.template($("#contacts-template").html()),
   mainTemplate: _.template($('#main-template').html()),
   initialize: function() {
     this.render();
@@ -12,19 +7,4 @@ var MainView = Backbone.View.extend({
   render: function() {
     this.$el.html(this.mainTemplate());
   },
-  showMain: function() {
-    $('#main').html(this.mainTemplate());
-  },
-  showProjects: function() {
-    $('#main').html(this.projectsTemplate());
-  },
-  showResume: function() {
-    $('#main').html(this.resumeTemplate());
-  },
-  showPhotography: function() {
-    $('#main').html(this.photographyTemplate());
-  },
-  showContacts: function() {
-    $('#main').html(this.contactsTemplate());
-  }
 });
