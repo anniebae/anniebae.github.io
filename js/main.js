0231ad18945eb41aa6ae49e43a2e1e27a0d79763
anniebae <bae.annie@gmail.com>
2015-06-01 17:25:59 -0400

    active class on navbar click

--- js/main.js
+++ js/main.js
@@ -3,27 +3,28 @@ $(function() {
     interval: 3000
   });
 
+  $('#ga-tab').on('click', function () {
+    console.log('i work');
+    $(this).parent().find('li active').removeClass('active');
+    $(this).addClass('active');
+  });
   $('#projects-tab').on('click', function() {
     console.log('i work');
     $(this).parent().find('li.active').removeClass('active');
     $(this).addClass('active');
   });
-
   $('#resume-tab').on('click', function() {
     $(this).parent().find('li.active').removeClass('active');
     $(this).addClass('active');
   });
-
   $('#photography-tab').on('click', function() {
     $(this).parent().find('li.active').removeClass('active');
     $(this).addClass('active');
   });
-
   $('#contacts-tab').on('click', function() {
     $(this).parent().find('li.active').removeClass('active');
     $(this).addClass('active');
   });
-
 });
 
 new AppView();
