93b08cb21855e5a5f5f644296521f64df77f41dd
anniebae <bae.annie@gmail.com>
2015-06-01 17:33:34 -0400

    merging with GAsection

--- js/backbone/views/gaView.js
+++ js/backbone/views/gaView.js
@@ -3,8 +3,21 @@ var GaView = Backbone.View.extend({
   gaTemplate: _.template($('#ga-template').html()),
   initialize: function() {
     this.render();
+    $('.project-caption').hide();
+  },
+  events: {
+    'mouseover .project-photo' : 'showCaption'
   },
   render: function() {
     this.$el.html(this.gaTemplate());
-  }
+    return this;
+  },
+  showCaption: function(e) {
+    console.log('show projects');
+    e.preventDefault();
+    $projectDiv = $(e.currentTarget);
+    $projectDiv.find('.project-caption').show().mouseleave(function() {
+      $('.project-caption').hide();
+    }); 
+  },
 });
