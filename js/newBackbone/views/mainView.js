c5d65d2807f17e991a2184f66ae626e47b93590d
anniebae <bae.annie@gmail.com>
2015-06-20 13:22:59 -0400

    click event on btn-photoraphy

--- js/newBackbone/views/mainView.js
+++ js/newBackbone/views/mainView.js
@@ -2,13 +2,15 @@ var MainView = Backbone.View.extend({
   el: "#body",
   mainTemplate: _.template($('#landing-template').html()),
   projectTemplate: _.template($('#project-template').html()),
+  photographyTemplate: _.template($('#photography-template').html()),
   initialize: function(){
     this.render();
     overlayTrigger();
   },
   events: {
-    'click #btn-projects' : 'showProjects',
-    'mouseover .project-div' : 'showCaption'
+    'click #btn-projects'    : 'showProjects',
+    'mouseover .project-div' : 'showCaption',
+    'click #btn-photography' : 'showPhotography'
   },
   render: function(){
     this.$el.html(this.mainTemplate());
@@ -25,8 +27,8 @@ var MainView = Backbone.View.extend({
     $projectDiv.find('.project-block').css('opacity', '0.8').mouseleave(function() {
       $('.project-block').css('opacity', '0.18');
     });
-
-
-
+  },
+  showPhotography: function() {
+    this.$el.html(this.photographyTemplate());
   }
  });
