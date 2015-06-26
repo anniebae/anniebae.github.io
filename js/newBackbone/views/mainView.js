c1fa622d94160653d28ef7eeb17e85c02d353416
anniebae <bae.annie@gmail.com>
2015-06-26 15:24:22 -0400

    merging juneUpdate for portfolio makeover

---
+++ js/newBackbone/views/mainView.js
@@ -0,0 +1,53 @@
+var MainView = Backbone.View.extend({
+  el: "#body",
+  mainTemplate: _.template($('#landing-template').html()),
+  projectTemplate: _.template($('#project-template').html()),
+  photographyTemplate: _.template($('#photography-template').html()),
+  resumeTemplate: _.template($('#resume-template').html()),
+  contactTemplate: _.template($('#contact-template').html()),
+  initialize: function(){
+    this.render();
+    overlayTrigger();
+  },
+  events: {
+    'click #btn-home'         : 'initialize',
+    'click #btn-projects'     : 'showProjects',
+    'mouseover .project-div'  : 'showCaption',
+    'click #btn-photography'  : 'showPhotography',
+    'click #btn-resume'       : 'showResume',
+    'click #btn-contact'      : 'showContact',
+    'mouseover .contact-icon' : 'showText'
+  },
+  render: function(){
+    this.$el.html(this.mainTemplate());
+  },
+  showProjects: function() {
+    this.$el.html(this.projectTemplate());
+    overlayTrigger();
+  },
+  showCaption: function(e) {
+    e.preventDefault();
+    $projectDiv = $(e.currentTarget);
+    $projectDiv.find('.project-text').show().mouseleave(function() {
+      $('.project-text').hide();
+    });
+    $projectDiv.find('.project-block').css('opacity', '0.8').mouseleave(function() {
+      $('.project-block').css('opacity', '0.18');
+    });
+  },
+  showPhotography: function() {
+    this.$el.html(this.photographyTemplate());
+    var slider = new WallopSlider('.wallop-slider');
+    overlayTrigger();
+    return this;
+  },
+  showResume: function() {
+    this.$el.html(this.resumeTemplate());
+    overlayTrigger();
+  },
+  showContact: function() {
+    this.$el.html(this.contactTemplate());
+    $('#tooltip').tooltip();
+    overlayTrigger();
+  }
+ });
