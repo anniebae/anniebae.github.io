631cfccdb6e46b45b5c9b25d2f4e940c5222080c
Annie Bae <bae.annie@gmail.com>
2015-08-01 20:01:44 -0400

    Update app.js

--- js/models/app.js
+++ js/models/app.js
@@ -1,6 +1,5 @@
 var Backdrop = Backbone.Model.extend({});
 var Link = Backbone.Model.extend({});
-
 // collections looking to take in an array of objects. ALWAYS an array.
 var BackdropCollection = Backbone.Collection.extend({
   model: Backdrop
