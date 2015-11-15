09029a1a7b598116bc79ca4a5beb69c798f1a31d
anniebae <bae.annie@gmail.com>
2015-11-15 01:29:18 -0500

    landing page created

--- js/main.js
+++ js/main.js
@@ -1,49 +1,13 @@
-new WOW().init();
-
-var overlayTrigger = function() {
-  var triggerBttn = document.getElementById( 'trigger-overlay' ),
-      overlay = document.querySelector( 'div.overlay' ),
-      closeBttn = overlay.querySelector( 'button.overlay-close' );
-      transEndEventNames = {
-          'WebkitTransition': 'webkitTransitionEnd',
-          'MozTransition': 'transitionend',
-          'OTransition': 'oTransitionEnd',
-          'msTransition': 'MSTransitionEnd',
-          'transition': 'transitionend'
-      },
-      transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-      support = { transitions : Modernizr.csstransitions };
-  function toggleOverlay() {
-      if( classie.has( overlay, 'open' ) ) {
-          classie.remove( overlay, 'open' );
-          classie.add( overlay, 'close' );
-          var onEndTransitionFn = function( ev ) {
-              if( support.transitions ) {
-                  if( ev.propertyName !== 'visibility' ) return;
-                  this.removeEventListener( transEndEventName, onEndTransitionFn );
-              }
-              classie.remove( overlay, 'close' );
-          };
-          if( support.transitions ) {
-              overlay.addEventListener( transEndEventName, onEndTransitionFn );
-          }
-          else {
-              onEndTransitionFn();
-          }
-      }
-      else if( !classie.has( overlay, 'close' ) ) {
-          classie.add( overlay, 'open' );
-      }
-  }
-  triggerBttn.addEventListener( 'click', toggleOverlay );
-  closeBttn.addEventListener( 'click', toggleOverlay );
-}
+console.log('hello world');
 
 $(function() {
-  $(window).load(function() {
-    $('#preloader').delay(500).fadeOut();
-    $('.preloader').delay(600).fadeOut('slow');
-  });
-});
+  // $('.bxslider').bxSlider();
 
-var mainView = new MainView();
+
+$('.slideshow').cycle({ 
+    fx:    'fade', 
+    speed:  3000 
+  });
+
+
+});
