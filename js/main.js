02c2b7fb2c7f9b9675c6fa78e7c90292b132b3bd
anniebae <bae.annie@gmail.com>
2016-01-24 21:47:28 -0500

    mobile responsive

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function() {
 
   $('.slideshow').cycle({ 
     fx:    'fade', 
-    speed:  3000 
+    speed:  2000 
   });
 
   var travelography = $('.travelography a').simpleLightbox();
@@ -12,6 +12,13 @@ $(function() {
   var whiskers = $('.whiskers a').simpleLightbox();
   var ix4 = $('.ix4 a').simpleLightbox();
   var hike = $('.hike a').simpleLightbox();
+
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: 'YOUR_CLIENT_ID'
+    });
+    feed.run();
 
 
 });
