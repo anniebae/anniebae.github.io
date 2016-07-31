1a7cd27b7ee60e6816dfcfcdde6514430bffe12e
anniebae <bae.annie@gmail.com>
2016-07-30 23:26:07 -0400

    about info rendered

--- app/components/About/index.js
+++ app/components/About/index.js
@@ -4,10 +4,50 @@ import Styles from './styles.scss';
 class About extends Component {
   render() {
     return (
-        <div>About DIV</div>
+        <div className="about-wrapper">
 
-      )
-  }
+
+            <div className="hello">
+              <div className="part">
+                <div className="part-title">Currently</div>
+                <div className="part-desc">
+                  New York City-based developer working at Tenthwave Digital LLC.      
+                </div>
+              </div>
+              
+              <div className="part">
+                <div className="part-title">In a nutshell</div>
+                <div className="part-desc">
+                  Alumna of Rutgers University (Communications & Digital Media) and General Assembly (Web Development Immersive). My interest in coding developed from working in the fashion industry, to creating ecommerce content, which eventually lead to building beautiful websites.
+                </div>
+              </div>
+              
+              <div className="part">
+                <div className="part-title">Things I like</div>
+                <div className="part-desc">
+                  Hobbies include <a href="http://abaebae.blogspot.com/" target="_blank">photography</a>, running, people-watching, and pestering my cat.
+                </div>
+              </div>
+              
+                
+                
+
+              <div className="part">
+                <div className="part-title">Stack</div>
+                <div className="part-desc">
+                  languages:  JavaScript, HTML5/CSS3, SASS
+                  libraries:  jQuery, Underscore
+                  frameworks:  Backbone, Angular
+                  tools:  BrowserStack, Litmus, MAMP, SourceTree, Transmit, Photoshop, FogBugz
+                  other:  Git, SVN, RWD/UI, Cross Browser Testing, Mobile Testing
+                </div>
+              </div>
+             
+            </div>
+        </div>
+
+        )
+    }
 }
 
 export default About;
