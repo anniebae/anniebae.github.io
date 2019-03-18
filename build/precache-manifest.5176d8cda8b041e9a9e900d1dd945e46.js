885f6ef9500b2825f5b7cc4f09deabee84252f77
anniebae <bae.annie@gmail.com>
2019-03-17 22:00:06 -0400

    reset

---
+++ build/precache-manifest.5176d8cda8b041e9a9e900d1dd945e46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9432d61c417b42951a21072db65fb2c0",
+    "url": "/anniebae.github.io/static/media/bnw.9432d61c.png"
+  },
+  {
+    "revision": "0a6092e8377dee1378a58cf5b9d696d0",
+    "url": "/anniebae.github.io/static/media/IMG_2392.0a6092e8.JPG"
+  },
+  {
+    "revision": "8134ee15be8c47d0b68d8625926266cd",
+    "url": "/anniebae.github.io/static/media/IMG_2390.8134ee15.JPG"
+  },
+  {
+    "revision": "55779429759845bf1990",
+    "url": "/anniebae.github.io/static/js/runtime~main.55779429.js"
+  },
+  {
+    "revision": "846b5a44c6ea074a7331",
+    "url": "/anniebae.github.io/static/js/main.846b5a44.chunk.js"
+  },
+  {
+    "revision": "1bbd409d23a489d7b89b",
+    "url": "/anniebae.github.io/static/js/1.1bbd409d.chunk.js"
+  },
+  {
+    "revision": "846b5a44c6ea074a7331",
+    "url": "/anniebae.github.io/static/css/main.3ab09468.chunk.css"
+  },
+  {
+    "revision": "e37bc8ae613868cf2e960dcb649b03bc",
+    "url": "/anniebae.github.io/index.html"
+  }
+];
