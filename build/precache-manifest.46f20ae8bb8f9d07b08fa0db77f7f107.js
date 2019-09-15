882ad9d5ce329e5f48107655d25faa420b0722c5
anniebae <bae.annie@gmail.com>
2019-09-15 16:53:18 -0400

    remove build from gitignore

---
+++ build/precache-manifest.46f20ae8bb8f9d07b08fa0db77f7f107.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66762a7457f72f3f48a60b519c35c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9bd002b5c99fc0db1bd",
+    "url": "/static/css/main.9755adc9.chunk.css"
+  },
+  {
+    "revision": "153557b7b9192d480517",
+    "url": "/static/js/2.6a49aa66.chunk.js"
+  },
+  {
+    "revision": "f9bd002b5c99fc0db1bd",
+    "url": "/static/js/main.0e7417e4.chunk.js"
+  },
+  {
+    "revision": "752ff289f84191a882cf",
+    "url": "/static/js/runtime~main.ac147d11.js"
+  },
+  {
+    "revision": "8134ee15be8c47d0b68d8625926266cd",
+    "url": "/static/media/IMG_2390.8134ee15.JPG"
+  },
+  {
+    "revision": "0a6092e8377dee1378a58cf5b9d696d0",
+    "url": "/static/media/IMG_2392.0a6092e8.JPG"
+  },
+  {
+    "revision": "9432d61c417b42951a21072db65fb2c0",
+    "url": "/static/media/bnw.9432d61c.png"
+  }
+]);
