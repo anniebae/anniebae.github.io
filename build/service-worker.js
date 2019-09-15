f6e12033e047b116b8596ab7bbdd1695811aebef
anniebae <bae.annie@gmail.com>
2019-09-15 16:11:16 -0400

    merge confict wip

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/anniebae.github.io/precache-manifest.5176d8cda8b041e9a9e900d1dd945e46.js"
+  "/precache-manifest.2f07ffa9a86e398de11355820954bc3e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/anniebae.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
